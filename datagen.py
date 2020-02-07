9962a063733ba536ad28ceb48816ab6f2c0a5449
Kevin Elgui <kevin.elgui@gmail.com>
2020-02-07 10:35:39 +0100

    Add files via upload

---
+++ datagen.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+a = 0.1
+d = 1
+n = 10000
+def sampling_linear_model(n, d, a):
+    X    = np.random.multivariate_normal(mean =np.zeros((d,)), 
+                                      cov     =np.eye(d),
+                                      size    =n
+                                     )
+    beta    = np.ones((d,1))/np.sqrt(d)
+    cov     = np.eye(2) + a*(np.ones((2,2)) - np.eye(2))
+    epsilon = np.random.multivariate_normal(mean=np.zeros((2,)), cov=cov, size=n)
+    Y1      = np.dot(X, beta).ravel() + epsilon[:,0]
+    Y2      = np.dot(X, beta).ravel() + epsilon[:,1]
+
+    return X, Y1, Y2, beta
+
+            
+    
+            
+
+        
